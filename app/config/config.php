f90e95bd412e50b62c08004ccb69e98ef8de80ad
Borivoje Lazarević <lazarevic.borivoje@gmail.com>
2019-12-17 10:56:57 +0100

    no message

--- app/config/config.php
+++ app/config/config.php
@@ -3,37 +3,37 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'e_diary1');
+define('DB_NAME', 'new_e_diary');
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/e_diary1');
+define('URLROOT', 'http://localhost/e_diary');
 // Site Name
-define('SITENAME', 'E-Diary1');
+define('SITENAME', 'E-Diary');
 // App Version
 define('APPVERSION', '1.0.0');
 
 // Admin URL Root
-define('URLROOT_admin', 'http://localhost/e_diary1/admin/admin');
+define('URLROOT_admin', 'http://localhost/new_e_diary/admin/admin');
 // Admin Site Name
 define('SITENAME_admin', 'Admin');
 
 // Parents URL Root
-define('URLROOT_parents', 'http://localhost/e_diary1/parents/parents');
+define('URLROOT_parents', 'http://localhost/new_e_diary/parents/parents');
 // Parents Site Name
 define('SITENAME_parents', 'Parent');
 
 // Principal URL Root
-define('URLROOT_principal', 'http://localhost/e_diary1/principal/principal');
+define('URLROOT_principal', 'http://localhost/new_e_diary/principal/principal');
 // Principal Site Name
 define('SITENAME_principal', 'Principal');
 
 // Proffesor URL Root
-define('URLROOT_proffesor', 'http://localhost/e_diary1/proffesor/proffesor');
+define('URLROOT_proffesor', 'http://localhost/new_e_diary/proffesor/proffesor');
 // Proffesor Site Name
 define('SITENAME_proffesor', 'Proffesor');
 
 // Teacher URL Root
-define('URLROOT_teacher', 'http://localhost/e_diary1/teacher/teacher');
+define('URLROOT_teacher', 'http://localhost/new_e_diary/teacher/teacher');
 // Teacher Site Name
 define('SITENAME_teacher', 'Teacher');
