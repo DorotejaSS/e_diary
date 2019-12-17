3718a065f990e5cb125e5a8e01ad725700a37b4b
Borivoje Lazarević <lazarevic.borivoje@gmail.com>
2019-12-17 11:16:49 +0100

    no message

--- app/config/config.php
+++ app/config/config.php
@@ -7,7 +7,7 @@ define('DB_NAME', 'new_e_diary');
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/e_diary');
+define('URLROOT', 'http://localhost/new_e_diary');
 // Site Name
 define('SITENAME', 'E-Diary');
 // App Version
