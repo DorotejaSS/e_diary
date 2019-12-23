bb064dc60f3ebf73142a10146f32c21ebf609dc1
Doroteja <doroteja.stst@gmail.com>
2019-12-23 12:11:30 +0100

    napravljeni view-i za svaku rolu koja se uloguje

--- app/models/User.php
+++ app/models/User.php
@@ -18,46 +18,69 @@ class User extends BaseModel
         // ako je request method post , udji u if
         if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-            var_dump($_SERVER['REQUEST_METHOD']);
-
             $email = $conn->escape_string($_POST['email']);
-            var_export($email);
             $password = $conn->escape_string($_POST['password']);
-            var_dump($password);
 
             $sql = 'select * from users where email = "'.$email.'" and password = "'.$password.'"';
-            var_dump($sql);
 
             $result = $conn->query($sql);
-            var_dump($result);
-            $result->fetch_assoc();
-            var_dump($result);
+            $user_data = $result->fetch_assoc();
+            var_dump($user_data);
+            
 
             // Ako se poklapaju sa podacima iz baze num_rows ce biti 1 
             // ako je 1, upisi email u Sesiju[login user] i pozovi view za korisnika (za sad je obican view koji pokazuje da si ulogovan)
             if ($result->num_rows == 1) {
-                $_SESSION['login_user'] = $email;
+                $_SESSION['login_user_email'] = $email;
+
+                switch ($user_data['role_id']) {
+                    case '1':
+                        $view = new View();
+                        $view->loadPage('admin', 'index');
+                        break;
+                     case '2':
+                        $view = new View();
+                        $view->loadPage('principal', 'index');
+                        break;
+                     case '3':
+                        $view = new View();
+                        $view->loadPage('professor', 'index');
+                        break;
+                     case '4':
+                        $view = new View();
+                        $view->loadPage('teacher', 'index');
+                        break;
+                     case '5':
+                        $view = new View();
+                        $view->loadPage('parent', 'index');
+                        break;
+                    
+                    default:
+                        echo '<h1>STRANA NIJE PRONADJENA</h1>';
+                        break;
+                }
+
                 $view = new View();
                 $view->loadPage('pages', 'welcome');
+                
             } else {
                 $error = "Your Email or Password is invalid";
                 echo $error;
             }
 
     
-            $user_check = $_SESSION['login_user'];
+            $user_check = $_SESSION['login_user_email'];
             var_dump($user_check);
    
-            $ses_sql = mysqli_query($conn, "select email from users where email = '$user_check'");
-            var_dump($ses_sql);
+            $ses_sql = mysqli_query($conn, "select * from users where email = '$user_check'");
             
             $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
-            var_dump($row);
-            
-            $login_session = $row['email'];
+
+            var_dump($_SESSION);         
+            $login_session['email'] = $row['email'];
             var_dump($login_session);
             
-            if(!isset($_SESSION['login_user'])){
+            if(!isset($_SESSION['login_user_email'])){
                 $view->loadPage('pages', 'login');
                 die;
             }
