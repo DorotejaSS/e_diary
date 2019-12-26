de7c525cd2889fa54ba582cc75a8606bdb788b78
Borivoje Lazarević <lazarevic.borivoje@gmail.com>
2019-12-26 12:06:17 +0100

    no message

--- app/config/routes.php
+++ app/config/routes.php
@@ -9,12 +9,8 @@ $routes = array(
     '/admin' => 'AdminController@homePage',
     '/users' => 'UserController@showAll',
     '/users/:id' => 'UserController@getOne',
-<<<<<<< HEAD
     '/parents' => 'ParentController@showChild',
     '/proffesor' => 'ProffesorController@homePage',
-=======
     '/users/:id/edit' => 'UserController@edit',
-    '/principal' => 'PrincipalController@homePage',
-    '/parents' => 'ParentController@showChild'
->>>>>>> 4536a5038a7a3593a37a6f4f225e0993bb1bd0ad
+    '/principal' => 'PrincipalController@homePage'
 );
