25c21f548dd6c88bc024f3fa8f5383837dbe8949
Luka Topalovic <topalovicluka00@gmail.com>
2019-12-27 11:20:24 +0100

    Dodat Menu model, uradjena navigacija

---
+++ app/models/Menu.php
@@ -0,0 +1,41 @@
+<?php
+
+class Menu extends BaseModel
+{
+    private $role_id;
+    private $result;
+    public $menu_title = array ();
+    public $menu_url = array ();
+
+    public function __construct()
+    {
+        if (isset($_SESSION['user_data']))
+        {
+            $this->role_id = $_SESSION['user_data']['role_id'];
+        }
+    }
+
+    public function getMenu()
+    {
+        require('./app/db.php');
+
+        if (isset($_SESSION['user_data']))
+        {
+            $sql= '
+            SELECT menu.title, menu.url 
+            FROM menu INNER JOIN role_menu 
+            ON menu.id = role_menu.menu_id 
+            WHERE role_menu.role_id = '.$this->role_id.'';
+            
+            $this->result = $conn->query($sql);
+            
+            $menu_data = $this->result->fetch_all();
+
+            foreach ($menu_data as $menu)
+            {
+                array_push ($this->menu_title, $menu[0]);
+                array_push ($this->menu_url, $menu[1]);
+            }
+        }
+    }
+}
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,1 +1,30 @@
 <h1>HEADER</h1>
+
+<?php
+
+if (isset($_SESSION['user_data']))
+{
+    $menu = new Menu;
+    $menu->getMenu();
+
+    echo '<ul>';
+
+    echo '<li> <a href="/logout"> Odjavi se </a> </li>';
+
+    for ($i=0; $i<count($menu->menu_title); $i++)
+    {
+        echo '<li> <a href="' . $menu->menu_url[$i] . '"> ' . $menu->menu_title[$i] . ' </a> </li>';
+    }
+
+    echo '</ul>';
+}
+else
+{
+    echo '
+    <ul>
+        <li> <a href="/login"> Uloguj se </a> </li>
+    </ul>
+    ';
+}
+
+?>
