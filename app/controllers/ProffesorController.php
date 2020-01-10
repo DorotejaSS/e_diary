7e405d8d01332b7659b652b4a0c2a19eefdaf883
Borivoje Lazarević <lazarevic.borivoje@gmail.com>
2020-01-10 10:10:08 +0100

    proba

---
+++ app/controllers/ProffesorController.php
@@ -0,0 +1,28 @@
+<?php
+
+    class ProffesorController extends BaseController 
+    {
+        protected $role_id = '3';
+
+        public function __construct()
+        {
+           
+            $this->checkSession();
+            if ($this->checkRole($this->role_id) === false)
+            {
+                echo 'NEMAS PRISTUP!';
+                exit;
+            }
+            var_dump(1);
+        }
+
+        public function homePage()
+        {
+            $user = new User();
+            $user->permissionTitles($this->role_id);
+            var_dump($user->permissionTitles($this->role_id));
+            $this->loadView('proffesor', 'index');
+            $this->loadView('pages', 'welcome');
+        }
+        
+    }
--- app/views/professor/index.php
+++ app/views/professor/index.php
@@ -1,3 +1,11 @@
 <?php
 
 echo '<h1>PROFESSOR PAGE</h1>';
+
+?>
+
+<button><a href="/diary">DIARY</a></button>
+<button><a href="/open_door">OPEN DOOR</a></button>
+<button><a href="/messages">MESSAGES</a></button>
+<button><a href="/schedule">SCHEDULE</a></button>
+<button><a href="/certificate">CERTIFICATE</a></button>
