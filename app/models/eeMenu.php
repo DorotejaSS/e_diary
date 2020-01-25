3d75a4013c2144a0e2637580423a322a6fb86bb3
Luka Topalovic <topalovicluka00@gmail.com>
2020-01-25 19:59:23 +0100

    konflikt resen

--- app/models/eeMenu.php
+++ app/models/eeMenu.php
@@ -18,18 +18,10 @@ class eeMenu extends BaseModel
     {
         require('./app/db.php');
 
-<<<<<<< HEAD
-        if (isset($_SESSION['user_data']))
-        {
-            $sql = $conn->prepare('SELECT menu.title, menu.url 
-                                   FROM menu INNER JOIN role_menu ON menu.id = role_menu.menu_id 
-                                   WHERE role_menu.role_id = :id');
-=======
         if (isset($_SESSION['user_data'])) {
             $sql = $conn->prepare('SELECT menu.title, menu.url 
                                     FROM menu INNER JOIN role_menu ON menu.id = role_menu.menu_id 
                                     WHERE role_menu.role_id = :id');
->>>>>>> 285d27a69272a5cc48a8ccd3a6e37820103aa988
 
             $sql->execute(array(':id' => $this->role_id));
 
